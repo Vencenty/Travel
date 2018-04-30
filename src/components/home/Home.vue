0d28b9f86bb11d91c7799637e31c93f6d3e1ec0d
Vencenty <yanchengtian0536@163.com>
2018-04-30 15:26:29 +0800

    add header

---
+++ src/components/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <home-header></home-header>
+</template>
+
+<script>
+  import HomeHeader from './Header'
+
+  export default {
+    name: "Home",
+    components: {
+      HomeHeader: HomeHeader
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import FastClick from 'fastclick'
+
+// 组件
+import Home from '@/components/home/Home'
+
+
+// 静态资源引入
+import 'static/reset.css'
+import 'static/border.css'
+import 'static/iconfont.css'
+
+FastClick.attach(document.body)
 
 Vue.use(Router)
-
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: Home
     }
   ]
 })
