de4e13e9c678dd0c54580ed74d64efe98bf34018
Vencenty <yanchengtian0536@163.com>
2018-05-01 10:40:25 +0800

    t

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,14 +1,20 @@
 <template>
-    <home-header></home-header>
+  <div>
+      <home-header></home-header>
+      <home-swiper></home-swiper>
+      <div>test</div>
+  </div>
 </template>
 
 <script>
   import HomeHeader from './Header'
+  import HomeSwiper from './Swiper'
 
   export default {
     name: "Home",
     components: {
-      HomeHeader: HomeHeader
+      HomeHeader,
+      HomeSwiper
     }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,21 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import router from './router' 
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+import FastClick from 'fastclick'
+
+// 静态资源引入
+import 'static/reset.css'
+import 'static/border.css'
+import 'static/iconfont.css'
+import 'swiper/dist/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper)
 
 Vue.config.productionTip = false
+FastClick.attach(document.body)
 
 new Vue({
   el: '#app',
